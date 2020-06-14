1676dfc5556edc81af9399c7633b056b155d7d9a
Tom Woodward <twwoodward@me.com>
2020-06-14 17:28:05 -0400

    add outline and outline link

--- single-course.php
+++ single-course.php
@@ -28,7 +28,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 					<h1><?php the_title();?></h1>
 				<!--DATE REPEATER-->	
 					<?php if( have_rows('dates') ): ?>
-						<div class="date-holder holder table-responsive">
+						<div class="date-holder holder table-responsive" id="dates">
 							<h2>Course Dates</h2>
 							<table class="table">
 									  <thead>
@@ -73,7 +73,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 						<?php endif; ?>
 				<!--END DATE REPEATER-->
 				<!--SHORT DESCRIPTION-->
-				<div class="short-holder holder">	
+				<div class="short-holder holder" id="short-desc">	
                     <?php 
 						$short_course_description = get_field('short_course_description');
 						echo '<h2>Short Course Description</h2><div class="short-description">' . $short_course_description . '</div>';
@@ -81,16 +81,26 @@ $container = get_theme_mod( 'understrap_container_type' );
 				</div>
 				<!--END SHORT DESCRIPTION-->
 				<!--LONG DESCRIPTION-->
-				<div class="long-holder holder">
+				<div class="long-holder holder" id="full-desc">
 					<?php 
 						$long_course_description = get_field('long_course_description');
 						echo '<h2>Long Course Description</h2><div class="long-description">' . $long_course_description . '</div>';
 					;?>
 				</div>
 				<!--END LONG DESCRIPTION-->
+				<!--COURSE OUTLINE-->
+				<div class="outline holder" id="outline">
+					<?php 
+						$course_outline = get_field('course_outline')['course_outline'];
+						$course_link = get_field('course_outline')['full_course_outline_link'];						
+						echo '<h2>Course Outline</h2><div class="course-outline">' . $course_outline . '</div>';
+						echo '<a href="' . $course_link .'" class="btn btn-primary">See the full outline</a>';
+					;?>
+				</div>
+				<!--END COURSE OUTLINE-->
 			<!--ARTEFACT REPEATER-->	
 					<?php if( have_rows('course_artefacts') ): ?>
-						<div class="artefact-holder holder">
+						<div class="artefact-holder holder" id="artefacts">
 							<h2>Course Artefacts</h2>
 		
 							<?php while( have_rows('course_artefacts') ): the_row(); 
@@ -108,7 +118,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 											<?php echo $artefact_title;?>
 										<?php endif; ?>
 										<?php if( $artefact_url ): ?>
-											<?php echo '</a> - ';?>
+											<?php echo '</a>';?>
 										<?php endif; ?>
 										<?php if( $artefact_desc ): ?>
 											<?php echo '<span class="artefact-desc">' . $artefact_desc . '</span>';?>
