9eb7e35e9cddb0dfeafacb2e01385f5792e9abe1
Tom Woodward <twwoodward@me.com>
2020-06-14 09:03:01 -0400

    shaping up single course display

--- single-course.php
+++ single-course.php
@@ -28,67 +28,76 @@ $container = get_theme_mod( 'understrap_container_type' );
 					<h1><?php the_title();?></h1>
 				<!--DATE REPEATER-->	
 					<?php if( have_rows('dates') ): ?>
-						<h2>Course Dates</h2>
-						<div class="row date-row">								
-							<div class="col-md-3">
-								<h3>Registration Start Date</h3>
+						<div class="date-holder holder">
+							<h2>Course Dates</h2>
+							<div class="row date-row">								
+								<div class="col-md-3">
+									<h3>Registration Start Date</h3>
+								</div>
+								<div class="col-md-3">
+									<h3>Registration End Date</h3>
+								</div>
+								<div class="col-md-3">
+									<h3>Course Start Date</h3>
+								</div>
+								<div class="col-md-3">
+									<h3>Course End Date</h3>
+								</div>
 							</div>
-							<div class="col-md-3">
-								<h3>Registration End Date</h3>
-							</div>
-							<div class="col-md-3">
-								<h3>Course Start Date</h3>
-							</div>
-							<div class="col-md-3">
-								<h3>Course End Date</h3>
-							</div>
-						</div>
-	
-						<?php while( have_rows('dates') ): the_row(); 
-							// vars
-							$reg_start = get_sub_field('registration_start_date');
-							$reg_end = get_sub_field('registration_end_date');
-							$course_start = get_sub_field('course_start_date');
-							$course_end = get_sub_field('course_end_date');
+		
+							<?php while( have_rows('dates') ): the_row(); 
+								// vars
+								$reg_start = get_sub_field('registration_start_date');
+								$reg_end = get_sub_field('registration_end_date');
+								$course_start = get_sub_field('course_start_date');
+								$course_end = get_sub_field('course_end_date');
 
-							?>
-							<div class="row">								
-								<div class="col-md-3">
-									<?php if( $reg_start ): ?>
-										<?php echo $reg_start; ?>
-									<?php endif; ?>
+								?>
+								<div class="row">								
+									<div class="col-md-3">
+										<?php if( $reg_start ): ?>
+											<?php echo $reg_start; ?>
+										<?php endif; ?>
+									</div>
+									<div class="col-md-3">
+										<?php if( $reg_end ): ?>
+											<?php echo $reg_end; ?>
+										<?php endif; ?>
+									</div>
+									<div class="col-md-3">
+										<?php if( $course_start ): ?>
+											<?php echo $course_start; ?>
+										<?php endif; ?>
+									</div>
+									<div class="col-md-3">
+										<?php if( $course_end ): ?>
+											<?php echo $course_end; ?>
+										<?php endif; ?>
+									</div>
 								</div>
-								<div class="col-md-3">
-									<?php if( $reg_end ): ?>
-										<?php echo $reg_end; ?>
-									<?php endif; ?>
-								</div>
-								<div class="col-md-3">
-									<?php if( $course_start ): ?>
-										<?php echo $course_start; ?>
-									<?php endif; ?>
-								</div>
-								<div class="col-md-3">
-									<?php if( $course_end ): ?>
-										<?php echo $course_end; ?>
-									<?php endif; ?>
-								</div>
-							</div>
-									
+										
 
-						<?php endwhile; ?>
+							<?php endwhile; ?>
 
-					<?php endif; ?>
-				<!--END DATE REPEATER-->	
+						<?php endif; ?>
+					</div>
+				<!--END DATE REPEATER-->
+				<!--SHORT DESCRIPTION-->
+				<div class="short-holder holder">	
                     <?php 
 						$short_course_description = get_field('short_course_description');
 						echo '<h2>Short Course Description</h2><div class="short-description">' . $short_course_description . '</div>';
 					;?>
-
+				</div>
+				<!--END SHORT DESCRIPTION-->
+				<!--LONG DESCRIPTION-->
+				<div class="long-holder holder">
 					<?php 
 						$long_course_description = get_field('long_course_description');
 						echo '<h2>Long Course Description</h2><div class="long-description">' . $long_course_description . '</div>';
 					;?>
+				</div>
+				<!--END LONG DESCRIPTION-->
 			<?php if (current_user_can( 'edit_post', $post->ID )) :?>
 				<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#editCourse" aria-expanded="false" aria-controls="collapseExample">
 			    	Edit/Update Your Course
