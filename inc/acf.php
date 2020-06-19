d5f308ed72fec7efa89fb921be09d6f8d84133b4
Tom Woodward <twwoodward@me.com>
2020-06-19 11:48:57 -0400

    faculty custom post type, acf additions

--- inc/acf.php
+++ inc/acf.php
@@ -92,7 +92,7 @@ function create_course_cpt() {
     'labels' => $labels,
     'menu_icon' => '',
     'supports' => array('title', 'editor', 'revisions', 'author', 'trackbacks', 'custom-fields', 'thumbnail',),
-    'taxonomies' => array(),
+    'taxonomies' => array('category','post_tag'),
     'public' => true,
     'show_ui' => true,
     'show_in_menu' => true,
@@ -106,7 +106,7 @@ function create_course_cpt() {
     'show_in_rest' => true,
     'publicly_queryable' => true,
     'capability_type' => 'post',
-    'menu_icon' => 'dashicons-universal-access-alt',
+    'menu_icon' => 'dashicons-admin-site-alt',
   );
   register_post_type( 'course', $args );
   
@@ -115,3 +115,70 @@ function create_course_cpt() {
   $wp_rewrite->flush_rules();
 }
 add_action( 'init', 'create_course_cpt', 0 );
+
+
+//faculty custom post type
+
+// Register Custom Post Type faculty
+// Post Type Key: faculty
+
+function create_faculty_cpt() {
+
+  $labels = array(
+    'name' => __( 'Faculty', 'Post Type General Name', 'textdomain' ),
+    'singular_name' => __( 'Faculty', 'Post Type Singular Name', 'textdomain' ),
+    'menu_name' => __( 'Faculty', 'textdomain' ),
+    'name_admin_bar' => __( 'Faculty', 'textdomain' ),
+    'archives' => __( 'Faculty Archives', 'textdomain' ),
+    'attributes' => __( 'Faculty Attributes', 'textdomain' ),
+    'parent_item_colon' => __( 'Faculty:', 'textdomain' ),
+    'all_items' => __( 'All Facultys', 'textdomain' ),
+    'add_new_item' => __( 'Add New Faculty', 'textdomain' ),
+    'add_new' => __( 'Add New', 'textdomain' ),
+    'new_item' => __( 'New Faculty', 'textdomain' ),
+    'edit_item' => __( 'Edit Faculty', 'textdomain' ),
+    'update_item' => __( 'Update Faculty', 'textdomain' ),
+    'view_item' => __( 'View Faculty', 'textdomain' ),
+    'view_items' => __( 'View Facultys', 'textdomain' ),
+    'search_items' => __( 'Search Faculty', 'textdomain' ),
+    'not_found' => __( 'Not found', 'textdomain' ),
+    'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+    'featured_image' => __( 'Featured Image', 'textdomain' ),
+    'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+    'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+    'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+    'insert_into_item' => __( 'Insert into faculty', 'textdomain' ),
+    'uploaded_to_this_item' => __( 'Uploaded to this faculty', 'textdomain' ),
+    'items_list' => __( 'Faculty list', 'textdomain' ),
+    'items_list_navigation' => __( 'Faculty list navigation', 'textdomain' ),
+    'filter_items_list' => __( 'Filter Faculty list', 'textdomain' ),
+  );
+  $args = array(
+    'label' => __( 'faculty', 'textdomain' ),
+    'description' => __( '', 'textdomain' ),
+    'labels' => $labels,
+    'menu_icon' => '',
+    'supports' => array('title', 'editor', 'revisions', 'author', 'trackbacks', 'custom-fields', 'thumbnail',),
+    'taxonomies' => array('category','post_tag'),
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+    'show_in_admin_bar' => true,
+    'show_in_nav_menus' => true,
+    'can_export' => true,
+    'has_archive' => true,
+    'hierarchical' => false,
+    'exclude_from_search' => false,
+    'show_in_rest' => true,
+    'publicly_queryable' => true,
+    'capability_type' => 'post',
+    'menu_icon' => 'dashicons-admin-users',
+  );
+  register_post_type( 'faculty', $args );
+  
+  // flush rewrite rules because we changed the permalink structure
+  global $wp_rewrite;
+  $wp_rewrite->flush_rules();
+}
+add_action( 'init', 'create_faculty_cpt', 0 );
