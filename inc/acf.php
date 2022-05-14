f47271b2121a0721c84363d2513ae2a130b6a313
woodwardtw <twwoodward@me.com>
2022-05-14 10:56:07 -0400

    acf work

--- inc/acf.php
+++ inc/acf.php
@@ -258,3 +258,168 @@ function create_university_taxonomies()
   ));
 }
 
+
+// Register Custom Post Type people
+// Post Type Key: university
+function create_university_cpt() {
+$labels = array(
+    'name' => __( 'University', 'Post Type General Name', 'textdomain' ),
+    'singular_name' => __( 'University', 'Post Type Singular Name', 'textdomain' ),
+    'menu_name' => __( 'University', 'textdomain' ),
+    'name_admin_bar' => __( 'University', 'textdomain' ),
+    'archives' => __( 'University Archives', 'textdomain' ),
+    'attributes' => __( 'University Attributes', 'textdomain' ),
+    'parent_item_colon' => __( 'University:', 'textdomain' ),
+    'all_items' => __( 'All Universities', 'textdomain' ),
+    'add_new_item' => __( 'Add New University', 'textdomain' ),
+    'add_new' => __( 'Add New', 'textdomain' ),
+    'new_item' => __( 'New University', 'textdomain' ),
+    'edit_item' => __( 'Edit University', 'textdomain' ),
+    'update_item' => __( 'Update University', 'textdomain' ),
+    'view_item' => __( 'View University', 'textdomain' ),
+    'view_items' => __( 'View Universities', 'textdomain' ),
+    'search_items' => __( 'Search Universities', 'textdomain' ),
+    'not_found' => __( 'Not found', 'textdomain' ),
+    'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+    'featured_image' => __( 'Featured Image', 'textdomain' ),
+    'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+    'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+    'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+    'insert_into_item' => __( 'Insert into university', 'textdomain' ),
+    'uploaded_to_this_item' => __( 'Uploaded to this university', 'textdomain' ),
+    'items_list' => __( 'University list', 'textdomain' ),
+    'items_list_navigation' => __( 'University list navigation', 'textdomain' ),
+    'filter_items_list' => __( 'Filter university list', 'textdomain' ),
+  );
+  $args = array(
+    'label' => __( 'university', 'textdomain' ),
+    'description' => __( '', 'textdomain' ),
+    'labels' => $labels,
+    'menu_icon' => '',
+    'supports' => array('title', 'editor', 'revisions', 'author', 'trackbacks', 'custom-fields', 'thumbnail',),
+    'taxonomies' => array('category', 'post_tag'),
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+    'show_in_admin_bar' => true,
+    'show_in_nav_menus' => true,
+    'can_export' => true,
+    'has_archive' => true,
+    'hierarchical' => false,
+    'exclude_from_search' => false,
+    'show_in_rest' => true,
+    'publicly_queryable' => true,
+    'capability_type' => 'post',
+    'menu_icon' => 'dashicons-welcome-learn-more',
+  );
+  register_post_type( 'university', $args );
+  
+  // flush rewrite rules because we changed the permalink structure
+  global $wp_rewrite;
+  $wp_rewrite->flush_rules();
+}
+add_action( 'init', 'create_university_cpt', 0 );
+
+
+//program custom post type
+
+// Register Custom Post Type program
+// Post Type Key: program
+
+function create_program_cpt() {
+
+  $labels = array(
+    'name' => __( 'Programs', 'Post Type General Name', 'textdomain' ),
+    'singular_name' => __( 'Program', 'Post Type Singular Name', 'textdomain' ),
+    'menu_name' => __( 'Program', 'textdomain' ),
+    'name_admin_bar' => __( 'Program', 'textdomain' ),
+    'archives' => __( 'Program Archives', 'textdomain' ),
+    'attributes' => __( 'Program Attributes', 'textdomain' ),
+    'parent_item_colon' => __( 'Program:', 'textdomain' ),
+    'all_items' => __( 'All Programs', 'textdomain' ),
+    'add_new_item' => __( 'Add New Program', 'textdomain' ),
+    'add_new' => __( 'Add New', 'textdomain' ),
+    'new_item' => __( 'New Program', 'textdomain' ),
+    'edit_item' => __( 'Edit Program', 'textdomain' ),
+    'update_item' => __( 'Update Program', 'textdomain' ),
+    'view_item' => __( 'View Program', 'textdomain' ),
+    'view_items' => __( 'View Programs', 'textdomain' ),
+    'search_items' => __( 'Search Programs', 'textdomain' ),
+    'not_found' => __( 'Not found', 'textdomain' ),
+    'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+    'featured_image' => __( 'Featured Image', 'textdomain' ),
+    'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+    'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+    'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+    'insert_into_item' => __( 'Insert into program', 'textdomain' ),
+    'uploaded_to_this_item' => __( 'Uploaded to this program', 'textdomain' ),
+    'items_list' => __( 'Program list', 'textdomain' ),
+    'items_list_navigation' => __( 'Program list navigation', 'textdomain' ),
+    'filter_items_list' => __( 'Filter Program list', 'textdomain' ),
+  );
+  $args = array(
+    'label' => __( 'program', 'textdomain' ),
+    'description' => __( '', 'textdomain' ),
+    'labels' => $labels,
+    'menu_icon' => '',
+    'supports' => array('title', 'editor', 'revisions', 'author', 'trackbacks', 'custom-fields', 'thumbnail',),
+    'taxonomies' => array('category', 'post_tag'),
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+    'show_in_admin_bar' => true,
+    'show_in_nav_menus' => true,
+    'can_export' => true,
+    'has_archive' => true,
+    'hierarchical' => false,
+    'exclude_from_search' => false,
+    'show_in_rest' => true,
+    'publicly_queryable' => true,
+    'capability_type' => 'post',
+    'menu_icon' => 'dashicons-universal-access-alt',
+  );
+  register_post_type( 'program', $args );
+  
+  // flush rewrite rules because we changed the permalink structure
+  global $wp_rewrite;
+  $wp_rewrite->flush_rules();
+}
+add_action( 'init', 'create_program_cpt', 0 );
+add_action( 'init', 'create_province_taxonomies', 0 );
+function create_province_taxonomies()
+{
+  // Add new taxonomy, NOT hierarchical (like tags)
+  $labels = array(
+    'name' => _x( 'Provinces', 'taxonomy general name' ),
+    'singular_name' => _x( 'province', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Provinces' ),
+    'popular_items' => __( 'Popular Provinces' ),
+    'all_items' => __( 'All Provinces' ),
+    'parent_item' => null,
+    'parent_item_colon' => null,
+    'edit_item' => __( 'Edit Provinces' ),
+    'update_item' => __( 'Update province' ),
+    'add_new_item' => __( 'Add New province' ),
+    'new_item_name' => __( 'New province' ),
+    'add_or_remove_items' => __( 'Add or remove Provinces' ),
+    'choose_from_most_used' => __( 'Choose from the most used Provinces' ),
+    'menu_name' => __( 'Province' ),
+  );
+
+//registers taxonomy specific post types - default is just post
+  register_taxonomy('provinces',array('post','program','course','university'), array(
+    'hierarchical' => true,
+    'labels' => $labels,
+    'show_ui' => false,
+    'update_count_callback' => '_update_post_term_count',
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'province' ),
+    'show_in_rest'          => true,
+    'rest_base'             => 'province',
+    'rest_controller_class' => 'WP_REST_Terms_Controller',
+    'show_in_nav_menus' => false,    
+  ));
+}
+
